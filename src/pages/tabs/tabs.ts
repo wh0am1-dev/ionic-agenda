17dccc597c7ea193933b7f0af8259750a8c108bd
neko250 <carlos.aguilar.112358@gmail.com>
2018-12-06 01:45:50 +0100

    fix: remove deprecated import

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
 @Component({
