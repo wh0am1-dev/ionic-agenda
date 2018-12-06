4bd1f350bb0ce089fd3178e8f3c132a362c6a5ce
neko250 <carlos.aguilar.112358@gmail.com>
2018-12-06 16:45:52 +0100

    feat: add contact filtering

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,13 +9,51 @@ import { ContactsProvider } from '../../providers/contacts/contacts';
 })
 export class HomePage {
 
+  contacts: object = {};
+
   constructor(
     public navCtrl: NavController,
     public contactsProv: ContactsProvider,
-  ) {}
+    ) {
+    this.contacts = contactsProv.getContacts();
+  }
 
   addContact() {
     console.log(':\')');
+  }
+
+  initials(): string[] {
+    let ini = [];
+
+    for (let k in this.contacts)
+      ini.push(k);
+
+    return ini;
+  }
+
+  filter(ev: any) {
+    let query = ev.target.value;
+    let keywords = query.split(/(?:,| )+/);
+    let contacts = this.contactsProv.getContacts();
+
+    if (query && query.trim() !== '') {
+      for (let ini in contacts) {
+        contacts[ini] = contacts[ini].filter(contact => {
+          let stays = false;
+
+          keywords.forEach(k => {
+            stays = stays || contact.name.toLowerCase().indexOf(k.toLowerCase()) > -1;
+            stays = stays || contact.surname.toLowerCase().indexOf(k.toLowerCase()) > -1;
+            stays = stays || contact.phone.toLowerCase().indexOf(k.toLowerCase()) > -1;
+            stays = stays || contact.email.toLowerCase().indexOf(k.toLowerCase()) > -1;
+          });
+
+          return stays;
+        });
+      }
+    }
+
+    this.contacts = contacts;
   }
 
 }
