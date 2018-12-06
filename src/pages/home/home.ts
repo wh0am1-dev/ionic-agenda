a7459a98f0e4dda8fd0530a9132222787f7ce9e7
neko250 <carlos.aguilar.112358@gmail.com>
2018-12-06 02:16:02 +0100

    feat: inject contacts provider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ContactsProvider } from '../../providers/contacts/contacts';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public contactsProv: ContactsProvider,
+  ) {}
 
+  addContact() {
+    console.log(':\')');
   }
 
 }
